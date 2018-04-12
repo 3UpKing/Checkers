014d0225dd196f0c1e6e767d2a4fc873bcc2b027
3UpKing <tavonga6000@gmail.com>
2018-04-12 23:15:37 +0200

    Start board with 12 black and 12 white pieces

--- checkers/Piece.cs
+++ checkers/Piece.cs
@@ -4,7 +4,49 @@ using System.Text;
 
 namespace checkers
 {
-    class Piece
+    public interface IPiece
     {
+        IEnumerable<int> NormalMoves(IBoard board);
+        IEnumerable<int> CapturingMoves(IBoard board);
+        Status Status { get; }
+        Color Color { get; }
+        int Position { get; }
+        void Move(int destination);
+    }
+
+    class Piece : IPiece
+    {
+        public Piece(Color c, int pos)
+        {
+            Status = Status.Active;
+            Position = pos;
+            Color = c;
+        }
+
+        public virtual Status Status { get; set; }
+        public virtual Color Color { get; private set; }
+        public virtual int Position { get; private set; }
+
+        public IEnumerable<int> CapturingMoves(IBoard board)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Move(int destination)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<int> NormalMoves(IBoard board)
+        {
+            throw new NotImplementedException();
+        }
+
+        public class King : Piece
+        {
+            public King(Color c, int p) : base(c, p) { }
+        }
+
+
     }
 }
