a0a68d572444586e330c9adb96ff34cee0c0d292
3UpKing <tavonga6000@gmail.com>
2018-04-12 14:59:45 +0200

    Initial Commit

---
+++ checkers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace checkers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
